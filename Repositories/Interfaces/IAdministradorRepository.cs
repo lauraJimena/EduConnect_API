ee2a5e92a0ffc9e9a0e57a8f3d6b7a1f9e431be1
LAURA JIMENA HERREÑO RUBIANO <lherreno@ucundinamarca.edu.co>
2025-09-24 21:28:11 -0500

    PB011-HU001, PB011-HU002, PB011-HU003, PB011-HU004: CRUD de usuarios para el rol administrador. Creación de endpoints y dtos de respuesta.

---
+++ Repositories/Interfaces/IAdministradorRepository.cs
@@ -0,0 +1,16 @@
+﻿using EduConnect_API.Dtos;
+
+namespace EduConnect_API.Repositories.Interfaces
+{
+
+        public interface IAdministradorRepository
+        {
+            Task<int> RegistrarUsuario(CrearUsuarioDto usuario);
+            Task<IEnumerable<ObtenerUsuarioDto>> ObtenerUsuarios();
+            Task<int> ActualizarUsuario(ActualizarUsuarioDto usuario);
+            Task<int> EliminarUsuario(int IdUsuario);
+           Task<ObtenerUsuarioDto?> ObtenerUsuarioPorId(int idUsuario);
+
+    }
+    
+}
---
+++ Services/Interfaces/IAdministradorService.cs
@@ -0,0 +1,15 @@
+﻿using EduConnect_API.Dtos;
+
+namespace EduConnect_API.Services.Interfaces
+{
+    public interface IAdministradorService
+    {
+        
+        Task RegistrarUsuario(CrearUsuarioDto usuario);
+        Task<IEnumerable<ObtenerUsuarioDto>> ObtenerUsuarios();
+        // Consultar por ID
+        Task<ObtenerUsuarioDto> ObtenerUsuarioPorId(int idUsuario);
+        Task<int> ActualizarUsuario(ActualizarUsuarioDto usuario);
+        Task<int> EliminarUsuario(int IdUsuario);
+    }
+}
